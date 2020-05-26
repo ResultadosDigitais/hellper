1f098cb7dd2ff9cd3037da0b0fc47d66de59b106
GLOFonseca <guilherme.fonseca@resultadosdigitais.com.br>
2020-05-26 11:56:27 -0300

    Criar método para inicializar a service de calendar

--- internal/calendar/google_calendar/calendar.go
+++ internal/calendar/google_calendar/calendar.go
@@ -1,12 +1,50 @@
 package googlecalendar
 
-import "hellper/internal/calendar"
+import (
+	"context"
+	"hellper/internal/calendar"
+	"hellper/internal/config"
+	googleauth "hellper/internal/google_auth"
+	"hellper/internal/log"
 
-type googleCalendar struct{}
+	gCalendar "google.golang.org/api/calendar/v3"
+)
+
+type googleCalendar struct {
+	logger          log.Logger
+	calendarService *gCalendar.Service
+}
 
 //NewCalendar initialize the file storage service
-func NewCalendar() calendar.Calendar {
-	return new(googleCalendar)
+func NewCalendar(ctx context.Context, logger log.Logger) calendar.Calendar {
+	calendarTokenBytes := []byte(config.Env.GoogleCalendarToken)
+
+	gClient, err := googleauth.Struct.GetGClient(ctx, logger, calendarTokenBytes, gCalendar.CalendarScope)
+	if err != nil {
+		logger.Error(
+			ctx,
+			"googlecalendar/calendar.NewCalendar GetGClient ERROR",
+			log.NewValue("error", err),
+		)
+
+		return nil
+	}
+
+	calendarService, err := gCalendar.New(gClient)
+	if err != nil {
+		logger.Error(
+			ctx,
+			"googlecalendar/calendar.NewCalendar gCalendar.New ERROR",
+			log.NewValue("error", err),
+		)
+
+		return nil
+	}
+
+	return &googleCalendar{
+		logger:          logger,
+		calendarService: calendarService,
+	}
 }
 
 //CreateCalendarEvent creates a event in Google Calendar

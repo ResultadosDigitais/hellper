b419c25b6b6eae38381991c00c8b454f7e90f57f
GLOFonseca <guilherme.fonseca@resultadosdigitais.com.br>
2020-05-27 16:12:39 -0300

    Create tests for event struct creation

--- internal/calendar/google_calendar/calendar_test.go
+++ internal/calendar/google_calendar/calendar_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	gCalendar "google.golang.org/api/calendar/v3"
 )
 
 type googleCalendarFixture struct {
@@ -13,10 +14,36 @@ type googleCalendarFixture struct {
 	errorMessage string
 
 	calendarService googleCalendar
+
+	startDateTime string
+	endDateTime   string
+	emails        []string
+	commander     string
+	summary       string
+	mockEvent     *gCalendar.Event
 }
 
 func (f *googleCalendarFixture) setup(t *testing.T) {
 
+}
+
+func TestEvent(t *testing.T) {
+	f := googleCalendarFixture{
+		startDateTime: `2020-05-27T16:00:00-07:00`,
+		endDateTime:   `2020-05-27T17:00:00-07:00`,
+		emails:        []string{},
+		commander:     `guilherme.fonseca@resultadosdigitais.com.br`,
+		summary:       `Test postmortem event`,
+		mockEvent:     newEventMock(),
+	}
+
+	t.Run("Create event struct", func(t *testing.T) {
+		event := event(f.startDateTime, f.endDateTime, f.summary, f.emails, f.commander)
+		ok := assert.Equal(t, f.mockEvent, event)
+		if !ok {
+			t.Fatal("fail")
+		}
+	})
 }
 
 func TestCreateCalendarEvent(t *testing.T) {
@@ -40,5 +67,23 @@ func TestCreateCalendarEvent(t *testing.T) {
 				t.Fatal("an error occurred, but was not expected")
 			}
 		})
+	}
+}
+
+func newEventMock() *gCalendar.Event {
+	return &gCalendar.Event{
+		Attendees: []*gCalendar.EventAttendee{
+			{
+				Email:     `guilherme.fonseca@resultadosdigitais.com.br`,
+				Organizer: true,
+			},
+		},
+		Start: &gCalendar.EventDateTime{
+			DateTime: `2020-05-27T16:00:00-07:00`,
+		},
+		End: &gCalendar.EventDateTime{
+			DateTime: `2020-05-27T17:00:00-07:00`,
+		},
+		Summary: `Test postmortem event`,
 	}
 }

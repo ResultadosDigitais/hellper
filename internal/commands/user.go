9698fbbbecca8bcf497342d56e24e1fcf2e509a3
GLOFonseca <guilherme.fonseca@resultadosdigitais.com.br>
2020-06-25 14:26:40 -0300

    Change file order

--- internal/commands/user.go
+++ internal/commands/user.go
@@ -38,31 +38,6 @@ func getSlackUserInfo(
 	return &user, err
 }
 
-func getUsersInConversation(
-	ctx context.Context,
-	client bot.Client,
-	logger log.Logger,
-	usersIDs *[]string,
-) (*[]model.User, error) {
-	var users []model.User
-
-	for _, id := range *usersIDs {
-		user, err := getSlackUserInfo(ctx, client, logger, id)
-		if err != nil {
-			logger.Error(
-				ctx,
-				"command/user.getUsersInConversation",
-				log.NewValue("channelID", usersIDs),
-				log.NewValue("error", err),
-			)
-			return nil, err
-		}
-		users = append(users, *user)
-	}
-
-	return &users, nil
-}
-
 func getUsersIDsInConversation(
 	ctx context.Context,
 	client bot.Client,
@@ -101,4 +76,29 @@ func getUsersIDsInConversation(
 	}
 
 	return &members, nil
+}
+
+func getUsersInConversation(
+	ctx context.Context,
+	client bot.Client,
+	logger log.Logger,
+	usersIDs *[]string,
+) (*[]model.User, error) {
+	var users []model.User
+
+	for _, id := range *usersIDs {
+		user, err := getSlackUserInfo(ctx, client, logger, id)
+		if err != nil {
+			logger.Error(
+				ctx,
+				"command/user.getUsersInConversation",
+				log.NewValue("channelID", usersIDs),
+				log.NewValue("error", err),
+			)
+			return nil, err
+		}
+		users = append(users, *user)
+	}
+
+	return &users, nil
 }

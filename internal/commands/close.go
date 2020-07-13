05a39542c0dfe3b2248a9dee4b164e5d4923effe
GLOFonseca <guilofonseca@gmail.com>
2020-07-13 15:25:07 -0300

    Add log on close command

--- internal/commands/close.go
+++ internal/commands/close.go
@@ -216,6 +216,13 @@ func CloseIncidentByDialog(ctx context.Context, client bot.Client, logger log.Lo
 	postAndPinMessage(client, channelID, "", channelAttachment)
 	err = client.ArchiveConversationContext(ctx, channelID)
 	if err != nil {
+		logger.Error(
+			ctx,
+			"command/close.CloseIncidentByDialog ArchiveConversationContext ERROR",
+			log.NewValue("channelID", channelID),
+			log.NewValue("userID", userID),
+			log.NewValue("error", err),
+		)
 		PostErrorAttachment(ctx, client, logger, channelID, userID, err.Error())
 		return err
 	}

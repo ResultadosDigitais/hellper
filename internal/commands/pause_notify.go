190adf96355a8a139b5bb3cc4b4a4513f5a6e513
Tiago Silva <tiagoemsi@gmail.com>
2020-07-15 18:28:38 -0300

    Removing user notification

--- internal/commands/pause_notify.go
+++ internal/commands/pause_notify.go
@@ -130,7 +130,7 @@ func PauseNotifyIncidentByDialog(
 		return err
 	}
 
-	postAndPinMessage(client, channelID, "Hellper notifications has been paused by <@"+userName+"> until *"+incident.SnoozedUntil.Time.Format(time.RFC1123)+"* for the following reason:\n```\n"+pauseNotifyReasonText+"\n```")
+	postAndPinMessage(client, channelID, "Hellper notifications has been paused by *"+userName+"* until *"+incident.SnoozedUntil.Time.Format(time.RFC1123)+"* for the following reason:\n```\n"+pauseNotifyReasonText+"\n```")
 	return nil
 }
 

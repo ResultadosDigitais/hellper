e750148c0384c883205b77e79119e5c0500e08cb
Tiago Silva <tiagoemsi@gmail.com>
2020-08-19 15:34:25 -0300

    Adding log.Logger

--- internal/bot/verify_requests.go
+++ internal/bot/verify_requests.go
@@ -2,35 +2,38 @@ package bot
 
 import (
 	"bytes"
+	"context"
 	"hellper/internal/config"
+	"hellper/internal/log"
 	"io/ioutil"
-	"log"
 	"net/http"
 
 	"github.com/slack-go/slack"
 )
 
 func VerifyRequests(r *http.Request, w http.ResponseWriter, f http.Handler) {
+	var logger log.Logger
+
 	secretVerifier, err := slack.NewSecretsVerifier(r.Header, config.Env.SlackSigningSecret)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(context.Background(), log.Trace(), log.Reason(err.Error()))
 	}
 
 	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(context.Background(), log.Trace(), log.Reason(err.Error()))
 	}
 
 	r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
 
 	_, err = secretVerifier.Write(body)
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(context.Background(), log.Trace(), log.Reason(err.Error()))
 	}
 
 	err = secretVerifier.Ensure()
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(context.Background(), log.Trace(), log.Reason(err.Error()))
 	}
 
 	f.ServeHTTP(w, r)

d025c5c2e9e752a152d8e5319a6164e46a5650bd
tiago.torresani <tiago.torresani@resultadosdigitais.com.br>
2020-10-23 15:14:31 -0300

    remove hellper language variable

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"strings"
-
 	"github.com/paked/configure"
 )
 
@@ -55,7 +53,6 @@ func newEnvironment() environment {
 		env  environment
 	)
 
-	vars.StringVar(&env.Language, "hellper_language", "pt-br", "Hellper languague")
 	vars.StringVar(&env.BindAddress, "hellper_bind_address", ":8080", "Hellper local bind address")
 	vars.StringVar(&env.MatrixHost, "hellper_matrix_host", "", "Matrix host")
 	vars.StringVar(&env.SupportTeam, "hellper_support_team", "", "Support team identifier")
@@ -84,68 +81,5 @@ func newEnvironment() environment {
 	vars.IntVar(&env.SLAHoursToClose, "hellper_sla_hours_to_close", 168, "SLA hours to close")
 
 	vars.Parse()
-	env.Messages = newMessages(env.Language)
 	return env
-}
-
-func newMessages(language string) messages {
-	language = strings.ToLower(language)
-	if language == "pt-br" {
-		return messages{
-			IncidentClosed:         "Incidente <#%s> encerrado.",
-			NoListOpenIncidents:    "Não há incidentes ativos!",
-			NoTimelineItems:        "Não há items na timeline",
-			AnswerAnIncident:       "Resposta a incidente: %s",
-			IncidentChannelCreated: "Canal do incidente criado: <#%s>",
-			BotHelp: `
-hellper
-Um bot para ajudar no tratamento de incidentes
-
-Comandos disponíves:
- help      Mostra esta mensagem
- ping      Testa a conectividade com o bot
- list      Lista os incidentes ativos
- state     Mostra a situação e a linha do tempo do incidente
-`,
-		}
-	}
-	if language == "en" {
-		return messages{
-			IncidentClosed:         "Incident <#%s> closed.",
-			NoListOpenIncidents:    "No active incidents!",
-			NoTimelineItems:        "No timeline items",
-			AnswerAnIncident:       "Response to incident: %s",
-			IncidentChannelCreated: "Channel of incident: <#%s>",
-			BotHelp: `
-hellper
-A bot to help the incident treatment
-
-Available commands:
- help      Show this help
- ping      Test bot connectivity
- list      List all active incidents
- state     Show incident state and timeline
-`,
-		}
-	}
-	if language == "es" {
-		return messages{
-			IncidentClosed:         "Incidente <#%s> cerrado.",
-			NoListOpenIncidents:    "No hay incidentes activos!",
-			NoTimelineItems:        "No timeline items",
-			AnswerAnIncident:       "Respuesta a incidente: %s",
-			IncidentChannelCreated: "Canal del incidente: <#%s>",
-			BotHelp: `
-hellper
-A bot to help the incident treatment
-
-Available commands:
- help      Show this help
- ping      Test bot connectivity
- list      List all active incidents
- state     Show incident state and timeline
-`,
-		}
-	}
-	return messages{}
 }
